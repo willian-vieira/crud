707a4b447711576ae138b40c3d8927bc131765e5
willian-vieira <williannvieira.w@gmail.com>
2023-04-02 13:30:45 -0300

    Interface de Pessoa criada com seus respectivos métodos definidos

--- src/dao/IPersonDAO.java
+++ src/dao/IPersonDAO.java
@@ -1,2 +1,16 @@
-package dao;public interface IPersonDAO {
+package dao;
+
+import model.Person;
+
+import java.util.List;
+
+/**
+ * Interface de Pessoa
+ */
+public interface IPersonDAO {
+    public List<Person> listPerson();
+    public Person findPersonById(Integer id);
+    public void addPerson(Person person);
+    public void updatePerson(Person person);
+    public void deletePerson(Integer id);
 }
