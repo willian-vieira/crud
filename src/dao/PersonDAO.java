d36ff6312657e10c3a36cd688f9239cdde52ad2c
willian-vieira <williannvieira.w@gmail.com>
2023-04-02 13:32:03 -0300

    Classe PersonDao que irá implementar os métodos definidos na interface IPersonDAO

--- src/dao/PersonDAO.java
+++ src/dao/PersonDAO.java
@@ -1,2 +1,57 @@
-package dao;public class PersonDAO {
+package dao;
+
+import model.Person;
+
+import java.util.List;
+
+/**
+ * Classe PersonDAO implementando Métodos definidos na interface IPersonDAO
+ */
+public class PersonDAO implements IPersonDAO {
+
+    /**
+     * Retorna lista de Pessoas
+     * @return
+     */
+    @Override
+    public List<Person> listPerson() {
+        return null;
+    }
+
+    /**
+     * Retorna uma Pessoa pelo ID
+     * @param id
+     * @return
+     */
+    @Override
+    public Person findPersonById(Integer id) {
+        return null;
+    }
+
+    /**
+     * Adiciona uma Pessoa
+     * @param person
+     */
+    @Override
+    public void addPerson(Person person) {
+
+    }
+
+    /**
+     * Atualiza uma Pessoa
+     * @param person
+     */
+    @Override
+    public void updatePerson(Person person) {
+
+    }
+
+    /**
+     * Deleta uma Pessoa
+     * @param id
+     */
+    @Override
+    public void deletePerson(Integer id) {
+
+    }
 }
