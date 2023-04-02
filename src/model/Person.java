025d7f43a79a814f82e96fd0f8ec2ece5fd5d415
willian-vieira <williannvieira.w@gmail.com>
2023-04-02 13:28:43 -0300

    Model de Pessoa

---
+++ src/model/Person.java
@@ -0,0 +1,51 @@
+package model;
+
+import java.math.BigInteger;
+
+public class Person {
+    Integer id;
+    String name;
+    String cpf;
+    String email;
+    String phone;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
