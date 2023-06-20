f32ca2423f34db3306627c9b50bcc92613598499
willian-vieira <williannvieira.w@gmail.com>
2023-06-20 18:37:17 -0300

    Classe Program - Inicialização da Aplicação

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,45 @@
 package application;
 
+import Utils.Impressao;
+import dao.PersonDAO;
+import model.Person;
+import service.PersonService;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
+        Person person = null;
+        PersonDAO personDAO = null;
+        PersonService personService = new PersonService();
+        Scanner scanner = new Scanner(System.in);
 
+        // --- Cabeçalho ---
+        System.out.println(Impressao.header());
+
+        // --- Menu ---
+        System.out.println(Impressao.menu());
+
+        // --- Captura seleção do Menu ---
+        int numberSelected = scanner.nextInt();
+
+        switch (numberSelected) {
+            case 1:
+                personService.listPeople();
+                break;
+            case 2:
+                personService.listOnePerson(null, null);
+                break;
+            case 3:
+                personService.updateAPerson(null, null, null);
+                break;
+            case 4:
+                personService.deleteAPerson(null, null);
+                break;
+            default:
+                System.out.println("Nenhuma das Opções Escolhidas!");
+                break;
+        }
     }
 }
